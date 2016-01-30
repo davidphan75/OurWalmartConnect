44f5c6e6f455b8e9a81604a4f78b52063204bb12
davidphan75 <davidphan75@gmail.com>
2016-01-29 16:51:29 -0800

    added frameworks

--- OurWalmartConnect/AppDelegate.swift
+++ OurWalmartConnect/AppDelegate.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import Parse
+import ParseFacebookUtilsV4
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,8 +18,22 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
+        
+        // PARSE
+        Parse.enableLocalDatastore()
+        Parse.setApplicationId("ltIEatWKlMcKO7hsMPcSwkOW8zKRPHfLj66pNQ3E", clientKey:"TLewYWhiWIlrc9UULHZ8NSaJGGC3QHkHfRECuC4C")
+        PFFacebookUtils.initializeFacebookWithApplicationLaunchOptions(launchOptions)
+        PFAnalytics.trackAppOpenedWithLaunchOptions(launchOptions)
+
+        
         return true
     }
+    
+    //Facebook
+    func application(application: UIApplication, openURL url: NSURL, sourceApplication: String?, annotation: AnyObject) -> Bool {
+        return FBSDKApplicationDelegate.sharedInstance().application(application, openURL: url, sourceApplication: sourceApplication, annotation: annotation)
+    }
+
 
     func applicationWillResignActive(application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
@@ -35,6 +51,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func applicationDidBecomeActive(application: UIApplication) {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+        
+        //Facebook
+        FBSDKAppEvents.activateApp()
+
     }
 
     func applicationWillTerminate(application: UIApplication) {
