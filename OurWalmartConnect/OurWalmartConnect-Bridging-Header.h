86c8f38dde511891bc43b0e462ded513a556ea05
davidphan75 <davidphan75@gmail.com>
2016-01-29 17:28:05 -0800

    added chat frameworks

---
+++ OurWalmartConnect/OurWalmartConnect-Bridging-Header.h
@@ -0,0 +1,29 @@
+//
+//  OurWalmartConnect-Bridging-Header.h
+//  OurWalmartConnect
+//
+//  Created by David Phan on 1/29/16.
+//  Copyright © 2016 David. All rights reserved.
+//
+
+//
+//  Use this file to import your target's public headers that you would like to expose to Swift.
+//
+
+#import <Parse/Parse.h>
+#import <ParseFacebookUtilsV4/PFFacebookUtils.h>
+#import "MBProgressHUD.h"
+#import "UIImageView+WebCache.h"
+#import <libxml/tree.h>
+#import <libxml/parser.h>
+#import <libxml/HTMLparser.h>
+#import <libxml/xpath.h>
+#import <libxml/xpathInternals.h>
+#import "JSQMessages.h"
+#import "JSQMessageData.h"
+#import "MGSwipeTableCell.h"
+#import "MGSwipeButton.h"
+#import "UIView+RNActivityView.h"
+#import "ParseUI/ParseUI.h"
+#import <Bolts/Bolts.h>
+#import <FBSDKCoreKit/FBSDKCoreKit.h>
